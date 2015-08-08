cd0b83d81746cbc6e493f3a87fe1f439993d3d5b
rafiq <email@rafiq.me>
2015-08-09 00:49:49 +0200

    max-min-long-lat-km-radius

--- max_min_lo_lat.c
+++ max_min_lo_lat.c
@@ -1,17 +1,29 @@
+// long & lat of a given area, i.e. 5 km.
 #include <stdio.h>
 #include <math.h>
 double deg2rad(double lat);
 int main()
 {
-double curr_long=-2.708077,curr_lat = 53.754842;
-int radius=20;
-double denominator = cos(deg2rad(curr_lat)) * 69;
+int eRadius = 12742;
+// current location here is Kiel, Germany.
+double curr_long=10.13625,curr_lat = 54.32168,cox_calc,dLon,dLat,cir,orig_distance;
+float radius=5;
+//each degree of lon or lat is approximately 69 miles or 111 kilometres difference
+// as it's not exactly 111 kilometres so you'll not get precision distance here.
+double denominator = cos(deg2rad(curr_lat)) * 111;
 double min_long = curr_long - (radius / denominator );
 double max_long = curr_long + (radius / denominator );
-double min_lat = curr_lat - (double)radius / 69;
-double max_lat = curr_lat + (double)radius / 69;
+double min_lat = curr_lat - (double)radius / 111;
+double max_lat = curr_lat + (double)radius / 111;
 printf("Minimum Longitude :%1.9f\nMaximum Longitude: %1.9f\n",min_long,max_long);
 printf("Minimum Latitude :%1.9f\nMaximum Latitude: %1.9f\n",min_lat,max_lat);
+//used haversine formula
+dLon = deg2rad(max_long)- deg2rad(min_long);
+dLat = deg2rad(max_lat) - deg2rad(min_lat);
+cox_calc = ((1 - cos(dLat)) + (1 - cos(dLon)) * cos(deg2rad(min_lat)) * cos(deg2rad(max_lat)));
+cox_calc = cox_calc / 2;
+orig_distance = eRadius * asin(sqrt(cox_calc));
+printf("If found, it could be original Distance: %1.7f\n",orig_distance);
 return 0;
 }
 
